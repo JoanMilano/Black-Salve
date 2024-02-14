667ede0f3d7d556538f56bdb6b54a16020aa1507
JoanMilano <147004601+JoanMilano@users.noreply.github.com>
2024-02-14 11:21:00 -0800

    finish Js for display
    finished all the javascript needed for displaying the order details. will begin properly link paypal now.

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,137 +1,115 @@
-import React, { useState } from "react";
-import twoOz from "../assets/imgs/open2oz.avif"
+import React, { useState, useEffect } from "react";
+import twoOz from "../assets/imgs/open2oz.avif";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Dropdown, DropdownDivider, DropdownItem, DropdownMenu, DropdownToggle } from "react-bootstrap";
 import { PayPalButton } from "react-paypal-button-v2";
 
-    const Checkout = () => {
 
-      const [selectedItems, setSelectedItems] = useState([]);
-      const [firstButtonText, setFirstButtonText] = useState("0");
-      const [secondButtonText, setSecondButtonText] = useState("0");
-      const [shippingPrice, setShippingPrice] = useState(0);
-      const [shippingType, setShippingType] = useState("Select a Shipping Method");
-      const [priceOfProductOnes, setPriceOfProductOnes] = useState(0);
-      const [priceOfProductTwos, setPriceOfProductTwos] = useState(0);
-      const [subTotalPrice, setSubTotalPrice] = useState(0);
-      const [totalPrice, setTotalPrice] = useState(0);
+// 1. dropdown wont go back to 0 
+const Checkout = () => {
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [firstButtonText, setFirstButtonText] = useState("0");
+  const [secondButtonText, setSecondButtonText] = useState("0");
+  const [shippingPrice, setShippingPrice] = useState(0);
+  const [shippingType, setShippingType] = useState("Select a Shipping Method");
+  const [priceOfProductOnes, setPriceOfProductOnes] = useState(0);
+  const [priceOfProductTwos, setPriceOfProductTwos] = useState(0);
+  const [subTotalPrice, setSubTotalPrice] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-
-      const productOne = {
+  const productOne = {
     title: '1oz',
     description: '1oz container of Black Salve',
     quantity: 0,
     price: 0,
-  }
+  };
 
   const productTwo = {
     title: '2oz',
     description: '2oz container of Black Salve',
     quantity: 0,
     price: 0,
-  }
+  };
 
- 
   const handleOnClick = (product, quantity, price) => {
-    // prevent default
     let selectedProduct = { title: product, quantity, price }; 
 
-    // checks which object the product  belongs too and assigns it to be selectedProduct
     if (product === productOne.title) {
       selectedProduct = { ...productOne, quantity, price }; 
     } else if (product === productTwo.title) {
       selectedProduct = { ...productTwo, quantity, price };
     }
-    // If selected product quantity is not 0 (if the if statement above is true)
-      if (selectedProduct) {
-        const existingItemIndex = selectedItems.findIndex((item) => item.title === selectedProduct.title);
-      
 
-
-        // based on the existingItemIndex var initialization above, this if () is if it already exist
-        // updates the quantity & price of that element in the array using the spread operator
-    if (existingItemIndex !== -1 && selectedProduct.quantity  > 0) {
-      setSelectedItems((prevItems) => 
-        prevItems.map((item, index) => 
-          index === existingItemIndex
-          ? { ...item, quantity, price} 
-          : item
-        )
-      );  // if the quantity is 0 remove it from the selectedItems array
-    } else if (selectedProduct.quantity === 0) {
-      setSelectedItems(selectedItems.filter(item => item.title !== selectedProduct.title));  
-    }
-    
-    
-    else {  // it deosnt already exist add to array using spread operator
-      setSelectedItems((prevItems) => 
-        [...prevItems, { ...selectedProduct, quantity, price}]
-      );
-    }
-
-
-    // update buttton text
-    if (product === productOne.title) {
-    setFirstButtonText(quantity) 
-  }  else if  (product === productTwo.title) {
-    setSecondButtonText(quantity)};  
-    
-  }
+    if (selectedProduct) {
+      const existingItemIndex = selectedItems.findIndex((item) => item.title === selectedProduct.title);
   
-  calculatePrices(selectedItems, shippingPrice, shippingType);
-  
+      if (existingItemIndex !== -1 && selectedProduct.quantity > 0) {
+        setSelectedItems((prevItems) => 
+          prevItems.map((item, index) => 
+            index === existingItemIndex ? { ...item, quantity, price } : item
+          )
+        );
+      } else if (selectedProduct.quantity === 0) {
+        setSelectedItems(selectedItems.filter((item) => item.title !== selectedProduct.title));  
+      } else {
+        setSelectedItems((prevItems) => [...prevItems, { ...selectedProduct, quantity, price }]);
+      }
+
+      if (product === productOne.title) {
+        setFirstButtonText(quantity);
+      } else if (product === productTwo.title) {
+        setSecondButtonText(quantity);
+      }
+    }
+  };
+
+  function calculateShipping(selectedItems, shippingType) {
+    const totalOrderQuantity = selectedItems.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+
+    if (totalOrderQuantity > 5) {  
+      setShippingType("Priority");
+      setShippingPrice(0); 
+      // automatically select priority for user
+      const priorityRadioButton = document.getElementById("option2");
+      if (priorityRadioButton) {
+        priorityRadioButton.click(); 
+      }
+    } else if (shippingType === "Priority" && totalOrderQuantity <= 5) {
+      setShippingPrice(5); 
+    }
   }
 
-// calculating shipping
-
-  function calculateShipping(selectedItems, shippingPrice, shippingType) {
-   
-  const totalOrderQuantity  = selectedItems.reduce((total, item)=> {
-    return total + item.quantity;
-  }, 0);
-      // !!!!!!!!Make radio buttons automatically select priority when order is over 5 items !!!!!!!!!!!!!
-  if (totalOrderQuantity > 5) {  
-  setShippingType("Priority")
-  setShippingPrice(0); 
-} else if (shippingType === "Priority" && totalOrderQuantity <= 5) {
-  setShippingPrice(5); 
-} else  {
-  setShippingType("Standard");
-  setShippingPrice(0);
-}
-  }
+  useEffect(() => {
+    calculatePrices(selectedItems, shippingPrice, shippingType);
 
   function calculatePrices(selectedItems, shippingPrice, shippingType) {
-    calculateShipping(selectedItems, shippingPrice, shippingType)
-    // total price of product one
+    calculateShipping(selectedItems, shippingPrice, shippingType);
+
     const priceOfProductOnes = selectedItems.filter(item => item.title === productOne.title).reduce((priceTotal, item) => {
       return priceTotal += item.quantity * item.price;
-      }, 0)
+    }, 0);
 
-    // total price of product two
-      const priceOfProductTwos = selectedItems.filter(item => item.title === productTwo.title).reduce((priceTotal, item) => {
-        return priceTotal += item.quantity * item.price;
-        }, 0)
+    const priceOfProductTwos = selectedItems.filter(item => item.title === productTwo.title).reduce((priceTotal, item) => {
+      return priceTotal += item.quantity * item.price;
+    }, 0);
 
-      // total price of items without shipping (subtotal)
-      const subTotalPrice = selectedItems.reduce((priceTotal, item) => {
-        return priceTotal += item.quantity * item.price; 
-      }, 0)
+    const subTotalPrice = selectedItems.reduce((priceTotal, item) => {
+      return priceTotal += item.quantity * item.price; 
+    }, 0);
 
+    const totalPrice = selectedItems.reduce((priceTotal, item) => {
+      return priceTotal += item.quantity * item.price + shippingPrice; 
+    }, 0);
 
-// total price of items with shipping (total)
-        const totalPrice = selectedItems.reduce((priceTotal, item) => {
-
-          return priceTotal += item.quantity * item.price + shippingPrice; 
-        }, 0)
-        console.log(selectedItems)
     setPriceOfProductOnes(priceOfProductOnes);
     setPriceOfProductTwos(priceOfProductTwos);
     setSubTotalPrice(subTotalPrice);
     setTotalPrice(totalPrice);
-       
-        
   }
+  // eslint-disable-next-line
+}, [selectedItems, shippingPrice, shippingType]); 
 
   const createOrder = (actions) => { 
     return actions.order.create({
@@ -178,12 +156,27 @@ import { PayPalButton } from "react-paypal-button-v2";
    className="btn-color"
    id="dropdown-split-basic" />
   <DropdownMenu className="drop-menu">
-    <DropdownItem onClick={() => setSelectedItems(selectedItems.filter(item => item.title !== productOne.title))} placeholder="0">None</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 1, 16.99)}>1 --1oz-- 17.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 0, 0.00)} placeholder="0">None</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 1, 17.99)}>1 --1oz-- 17.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productOne.title, 2, 16.99)}>2 --1oz-- 16.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productOne.title, 3, 16.99)}>3 --1oz-- 16.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productOne.title, 4, 16.99)}>4 --1oz-- 16.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productOne.title, 5, 16.99)}>5 --1oz-- 16.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 6, 15.99)}>6 --1oz-- 15.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 7, 15.99)}>7 --1oz-- 15.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 8, 15.99)}>8 --1oz-- 15.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 9, 15.99)}>9 --1oz-- 15.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 10, 14.99)}>10 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 11, 14.99)}>11 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 12, 14.99)}>12 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 13, 14.99)}>13 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 14, 14.99)}>14 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 15, 14.99)}>15 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 16, 14.99)}>16 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 17, 14.99)}>17 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 18, 14.99)}>18 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 19, 14.99)}>19 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 20, 14.99)}>20 --1oz-- 14.99$ ea.</DropdownItem>
     <DropdownDivider></DropdownDivider>
     <DropdownItem>Contact for Wholesale</DropdownItem>
   </DropdownMenu>
@@ -199,12 +192,27 @@ import { PayPalButton } from "react-paypal-button-v2";
    className="btn-color"
    id="dropdown-split-basic" />
   <DropdownMenu className="drop-menu">
-  <DropdownItem onClick={() => setSelectedItems(selectedItems.filter(item => item.title !== productTwo.title))} placeholder="0">None</DropdownItem>
+  <DropdownItem onClick={() => handleOnClick(productTwo.title, 0, 0.00)} placeholder="0">None</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productTwo.title, 1, 24.99)}>1 --2oz-- 24.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productTwo.title, 2, 23.99)}>2 --2oz-- 23.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productTwo.title, 3, 23.99)}>3 --2oz-- 23.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productTwo.title, 4, 23.99)}>4 --2oz-- 23.99$ ea.</DropdownItem>
     <DropdownItem onClick={() => handleOnClick(productTwo.title, 5, 23.99)}>5 --2oz-- 23.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 6, 22.99)}>6 --2oz-- 22.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 7, 22.99)}>7 --2oz-- 22.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 8, 22.99)}>8 --2oz-- 22.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 9, 22.99)}>9 --2oz-- 22.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 10, 21.99)}>10 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 11, 21.99)}>11 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 12, 21.99)}>12 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 13, 21.99)}>13 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 14, 21.99)}>14 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 15, 21.99)}>15 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 16, 21.99)}>16 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 17, 21.99)}>17 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 18, 21.99)}>18 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 19, 21.99)}>19 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 20, 21.99)}>20 --2oz-- 21.99$ ea.</DropdownItem>
     <DropdownDivider></DropdownDivider> {/* up to 20 items*/}
     <DropdownItem>Contact for WholeSale</DropdownItem> 
   </DropdownMenu>
@@ -229,11 +237,11 @@ import { PayPalButton } from "react-paypal-button-v2";
  </section>
  <section className="details-section">
   <h3>Order Details</h3>
-    <p>{selectedItems.find(item => item.title === productOne.title)?.quantity || 0} 1oz {priceOfProductOnes}$</p>
-     <p>{selectedItems.find(item => item.title === productTwo.title)?.quantity || 0} 2oz {priceOfProductTwos}$</p>  {/* conditionally render if theres a quantity > 0*/ }
-    <p>Subtotal: {subTotalPrice}$</p>
+    <p>{selectedItems.find(item => item.title === productOne.title)?.quantity || 0} 1oz {priceOfProductOnes.toFixed(2)}$</p>
+     <p>{selectedItems.find(item => item.title === productTwo.title)?.quantity || 0} 2oz {priceOfProductTwos.toFixed(2)}$</p>  {/* conditionally render if theres a quantity > 0*/ }
+    <p>Subtotal: {subTotalPrice.toFixed(2)}$</p>
     <p>{shippingType}: {shippingPrice}$</p>
-    <p>Total {totalPrice}$</p>
+    <p>Total {totalPrice.toFixed(2)}$</p> {/*cap it off after the .00 so its not .000000000000000 */}
  </section>
  </div>
  <section className="paypal-section">
