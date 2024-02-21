5b6eff7cc6249918aae18e33c416609ce22e1069
JoanMilano <147004601+JoanMilano@users.noreply.github.com>
2024-02-21 13:11:34 -0800

    default

--- Back-end/Payment.js
+++ Back-end/Payment.js
@@ -1,1 +1,181 @@
 // send order confirmation email, and order ID 
+
+// send order confirmation email, and order ID 
+// take, parse, formatt and send API to paypal
+
+//initialize 
+const express = require('express');
+const bodyParser = require('body-parser');
+const paypal = require('paypal-rest-sdk');
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path')
+const app = express();
+
+
+//Configure
+app.use(cors({
+    origin: 'http://localhost:3001'
+  }));
+// Configure PayPal SDK
+const PAYPAL_CLIENT_ID = process.env.CLIENT_ID;
+const PAYPAL_CLIENT_SECRET = process.env.CLIENT_SECRET;
+const base = "https://api-m.sandbox.paypal.com";
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+
+
+// host static files
+app.use(express.static("client"));
+
+// parse post params sent in body in json format
+app.use(express.json());
+
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
+
+
+// generating access token make sure client and secret are correct 
+const generateAccessToken = async () => {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(
+      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET,
+    ).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+  }
+};
+
+
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+
+
+const createOrder = async (cart) => {
+  // use the cart information passed from the front-end to calculate the purchase unit details
+  console.log(
+    "shopping cart information passed from the frontend createOrder() callback:",
+    cart,
+  );
+
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const payload = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: cart.price,
+        },
+      },
+    ],
+  };
+
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  return handleResponse(response);
+};
+
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (orderID) => {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
+
+  return handleResponse(response);
+};
+
+async function handleResponse(response) {
+  try {
+    const jsonResponse = await response.json();
+    return {
+      jsonResponse,
+      httpStatusCode: response.status,
+    };
+  } catch (err) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
+
+app.post("/api/orders", async (req, res) => {
+  try {
+    // use the cart information passed from the front-end to calculate the order amount detals
+    const { cart } = req.body;
+    const { jsonResponse, httpStatusCode } = await createOrder(cart);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to create order." });
+  }
+});
+
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  try {
+    const { orderID } = req.params;
+    const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to capture order." });
+  }
+});
+
+// serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/index.html"));
+});
+
+PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Node server listening at http://localhost:${PORT}/`);
+});
--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,286 +1,125 @@
-import React, { useState, useEffect } from "react";
-import twoOz from "../assets/imgs/open2oz.avif";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Dropdown, DropdownDivider, DropdownItem, DropdownMenu, DropdownToggle } from "react-bootstrap";
-import { PayPalButton } from "react-paypal-button-v2";
+import React, { useState } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
 
 
 // 1.  order id is undefined
 const Checkout = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [firstButtonText, setFirstButtonText] = useState("0");
-  const [secondButtonText, setSecondButtonText] = useState("0");
-  const [shippingPrice, setShippingPrice] = useState(0);
-  const [shippingType, setShippingType] = useState("Select a Shipping Method");
-  const [priceOfProductOnes, setPriceOfProductOnes] = useState(0);
-  const [priceOfProductTwos, setPriceOfProductTwos] = useState(0);
-  const [subTotalPrice, setSubTotalPrice] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const productOne = {
-    title: '1oz',
-    description: '1oz container of Black Salve',
-    quantity: 0,
-    price: 0,
-  };
 
-  const productTwo = {
-    title: '2oz',
-    description: '2oz container of Black Salve',
-    quantity: 0,
-    price: 0,
-  };
+  // Renders errors or successfull transactions on the screen.
+  function Message({ content }) {
+    return <p>{content}</p>;
+  }
 
-  const handleOnClick = (product, quantity, price) => {
-    let selectedProduct = { title: product, quantity, price }; 
-
-    if (product === productOne.title) {
-      selectedProduct = { ...productOne, quantity, price }; 
-    } else if (product === productTwo.title) {
-      selectedProduct = { ...productTwo, quantity, price };
-    }
-
-    if (selectedProduct) {
-      const existingItemIndex = selectedItems.findIndex((item) => item.title === selectedProduct.title);
+    const initialOptions = {
+      "client-id": "AVeXX3FDtEFSDdECuQIhy2r6hQ--wZkU9o77sFL-uUkCgX7jWlihrErOOyHFpFdyzxWuOwk6xmx7-gLx",
+      "enable-funding": "card",
+      "disable-funding": "paylater,venmo",
+      "data-sdk-integration-source": "integrationbuilder_sc",
+    };
   
-      if (existingItemIndex !== -1 && selectedProduct.quantity > 0) {
-        setSelectedItems((prevItems) => 
-          prevItems.map((item, index) => 
-            index === existingItemIndex ? { ...item, quantity, price } : item
-          )
-        );
-      } else if (selectedProduct.quantity === 0) {
-        setSelectedItems(selectedItems.filter((item) => item.title !== selectedProduct.title));  
-      } else {
-        setSelectedItems((prevItems) => [...prevItems, { ...selectedProduct, quantity, price }]);
-      }
-
-      if (product === productOne.title) {
-        setFirstButtonText(quantity);
-      } else if (product === productTwo.title) {
-        setSecondButtonText(quantity);
-      }
-    }
-  };
-
-  function calculateShipping(selectedItems) {
-    const totalOrderQuantity = selectedItems.reduce((total, item) => {
-      return total + item.quantity;
-    }, 0);
-
-
-    if (totalOrderQuantity > 5) {  
-      setShippingPrice(0); 
-    setShippingType("Priority");
-      // automatically select priority for user
-      const priorityRadioButton = document.getElementById("option2");
-      if (priorityRadioButton) {
-        priorityRadioButton.click(); 
-      }
-    } else if (shippingType === "Priority" && totalOrderQuantity <= 5) {
-      setShippingPrice(5); 
-    } else {
-      setShippingPrice(0); 
-    }
-  }
-
-  useEffect(() => {
-    calculatePrices(selectedItems, shippingPrice, shippingType);
-
-  function calculatePrices(selectedItems, shippingPrice, shippingType) {
-    calculateShipping(selectedItems, shippingPrice, shippingType);
-
-    const priceOfProductOnes = selectedItems.filter(item => item.title === productOne.title).reduce((priceTotal, item) => {
-      return priceTotal + (item.quantity * item.price);
-    }, 0);
-
-    const priceOfProductTwos = selectedItems.filter(item => item.title === productTwo.title).reduce((priceTotal, item) => {
-      return priceTotal += item.quantity * item.price;
-    }, 0);
-
-    const subTotalPrice = selectedItems.reduce((priceTotal, item) => {
-      return priceTotal += item.quantity * item.price; 
-    }, 0);
-
-    const totalPrice = selectedItems.reduce((priceTotal, item) => {
-      return priceTotal += item.quantity * item.price + shippingPrice; 
-    }, 0);
-
-    setPriceOfProductOnes(priceOfProductOnes);
-    setPriceOfProductTwos(priceOfProductTwos);
-    setSubTotalPrice(subTotalPrice.toFixed(2));
-    setTotalPrice(totalPrice.toFixed(2));
-  }
-  // eslint-disable-next-line
-}, [selectedItems, shippingPrice, shippingType]); 
-
-
-const createOrder = ( actions ) => { 
-  const itemTotal = selectedItems.reduce((priceTotal, item) => {
-    return priceTotal += item.quantity * item.price; 
-  }, 0);
-  return actions.order.create({
-    application_context: {
-      locale: 'en-US'
-    },
-    purchase_units: [
-      {
-        amount: {
-          currency_code: 'USD',
-          value: totalPrice,
-          breakdown: {
-            item_total: {
-              currency_code: 'USD',
-              value: itemTotal
-             }, // set item_total to a number value
-             shipping: {
-              currency_code: 'USD',
-              value: shippingPrice
-            }
-            }
-            },
-        items: selectedItems.map(item => ({
-            name: item.title,
-            description: item.description,
-            quantity: item.quantity,
-            category: 'PHYSICAL_GOODS',
-            unit_amount: {
-              currency_code: 'USD',
-              value: item.price
-            },
-          }))
-          
-      },
-    ],
-  });
-}; 
-
-const handleOnSuccess = (details) => {
-  const { payer } = details;
-  const customerName = payer.name.given_name;
-  const customerOrderID =  details.orderID;
-  alert(
-    `Transaction complete! Thank You for your purchase ${customerName}, your order ID is ${customerOrderID}`
-  );
+    const [message, setMessage] = useState("");
+  
+    return (
+      <div className="App">
+        <PayPalScriptProvider options={initialOptions}>
+          <PayPalButtons
+            style={{
+              shape: "pill",
+              layout: "vertical",
+            }}
+            createOrder={async () => {
+              try {
+                const response = await fetch("/api/orders", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  // use the "body" param to optionally pass additional order information
+                  // like product ids and quantities
+                  body: JSON.stringify({
+                    cart: [
+                      {
+                        id: "itemOne",
+                        quantity: 1,
+                        price: 100.00
+                      },
+                    ],
+                  }),
+                });
+  
+                const orderData = await response.json();
+  
+                if (orderData.id) {
+                  return orderData.id;
+                } else {
+                  const errorDetail = orderData?.details?.[0];
+                  const errorMessage = errorDetail
+                    ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                    : JSON.stringify(orderData);
+  
+                  throw new Error(errorMessage);
+                }
+              } catch (error) {
+                console.error(error);
+                setMessage(`Could not initiate PayPal Checkout...${error}`);
+              }
+            }}
+            onApprove={async (data, actions) => {
+              try {
+                const response = await fetch(
+                  `/api/orders/${data.orderID}/capture`,
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  },
+                );
+  
+                const orderData = await response.json();
+                // Three cases to handle:
+                //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                //   (2) Other non-recoverable errors -> Show a failure message
+                //   (3) Successful transaction -> Show confirmation or thank you message
+  
+                const errorDetail = orderData?.details?.[0];
+  
+                if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                  // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                  // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                  return actions.restart();
+                } else if (errorDetail) {
+                  // (2) Other non-recoverable errors -> Show a failure message
+                  throw new Error(
+                    `${errorDetail.description} (${orderData.debug_id})`,
+                  );
+                } else {
+                  // (3) Successful transaction -> Show confirmation or thank you message
+                  // Or go to another URL:  actions.redirect('thank_you.html');
+                  const transaction =
+                    orderData.purchase_units[0].payments.captures[0];
+                  setMessage(
+                    `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`,
+                  );
+                  console.log(
+                    "Capture result",
+                    orderData,
+                    JSON.stringify(orderData, null, 2),
+                  );
+                }
+              } catch (error) {
+                console.error(error);
+                setMessage(
+                  `Sorry, your transaction could not be processed...${error}`,
+                );
+              }
+            }}
+          />
+        </PayPalScriptProvider>
+        <Message content={message} />
+      </div>
+    );
 }
 
-
-    return (
-      <div className="checkout-page">
-      <div className="checkout-title"><h2>Checkout</h2></div>
-
-      <section className="salve-option-one">
-      <img src={twoOz} alt="1oz tin of Black Salve" />
-      <Dropdown className="dropdown">
-  <button variant="info" className="btn btn-color btn-text">{firstButtonText}</button>
-  <DropdownToggle
-   split 
-   className="btn-color"
-   id="dropdown-split-basic" />
-  <DropdownMenu className="drop-menu">
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 0, 0.00)} placeholder="0">None</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 1, 17.99)}>1 --1oz-- 17.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 2, 16.99)}>2 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 3, 16.99)}>3 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 4, 16.99)}>4 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 5, 16.99)}>5 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 6, 15.99)}>6 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 7, 15.99)}>7 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 8, 15.99)}>8 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 9, 15.99)}>9 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 10, 14.99)}>10 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 11, 14.99)}>11 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 12, 14.99)}>12 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 13, 14.99)}>13 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 14, 14.99)}>14 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 15, 14.99)}>15 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 16, 14.99)}>16 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 17, 14.99)}>17 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 18, 14.99)}>18 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 19, 14.99)}>19 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 20, 14.99)}>20 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownDivider></DropdownDivider>
-    <DropdownItem>Contact for Wholesale</DropdownItem>
-  </DropdownMenu>
-</Dropdown>
-</section>
-
-<section className="salve-option-two">
-<img src={twoOz} alt="2oz tin of Black Salve" />
-<Dropdown className="dropdown">
-  <button variant="info" className="btn btn-color btn-text">{secondButtonText}</button>
-  <DropdownToggle
-   split 
-   className="btn-color"
-   id="dropdown-split-basic" />
-  <DropdownMenu className="drop-menu">
-  <DropdownItem onClick={() => handleOnClick(productTwo.title, 0, 0.00)} placeholder="0">None</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 1, 24.99)}>1 --2oz-- 24.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 2, 23.99)}>2 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 3, 23.99)}>3 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 4, 23.99)}>4 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 5, 23.99)}>5 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 6, 22.99)}>6 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 7, 22.99)}>7 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 8, 22.99)}>8 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 9, 22.99)}>9 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 10, 21.99)}>10 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 11, 21.99)}>11 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 12, 21.99)}>12 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 13, 21.99)}>13 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 14, 21.99)}>14 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 15, 21.99)}>15 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 16, 21.99)}>16 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 17, 21.99)}>17 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 18, 21.99)}>18 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 19, 21.99)}>19 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 20, 21.99)}>20 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownDivider></DropdownDivider> {/* up to 20 items*/}
-    <DropdownItem>Contact for WholeSale</DropdownItem> 
-  </DropdownMenu>
-</Dropdown>
-
- </section>
- <div className="shipping-details-pay">
-  <div className="shipping-details">
- <section className="shipping-section">
-      <form className="shipping-form" required> 
-        <div>
-      <input type="radio" id="option1" name="options" value="Standard" onClick={() => setShippingType('Standard') }/>
-  <label htmlFor="option1">Standard Shipping</label>
-  <p>Free - 5 to 7 bussiness days</p>
-  </div>
-  <div className="shipping-option-two">
-  <input type="radio" id="option2" name="options" value="Priority" onClick={() => setShippingType('Priority') }/>
-  <label htmlFor="option2">Priority Shipping</label>
-  <p>5$ or Free for orders over 5 items!</p>
-  <p>3 to 5 bussiness days</p>
-  </div>
-      </form>
- </section>
- <section className="details-section">
-  <h3>Order Details</h3>
-    <p>{selectedItems.find(item => item.title === productOne.title)?.quantity || 0} 1oz {priceOfProductOnes.toFixed(2)}$</p>
-     <p>{selectedItems.find(item => item.title === productTwo.title)?.quantity || 0} 2oz {priceOfProductTwos.toFixed(2)}$</p>  {/* conditionally render if theres a quantity > 0*/ }
-    <p>Subtotal: {subTotalPrice}$</p>
-    <p>{shippingType}: {shippingPrice}$</p>
-    <p>Total {totalPrice}$</p>
- </section>
- </div>
- <section className="paypal-section">
-  <h1>Purchase here!</h1>
-  {selectedItems.reduce((total, item) => total + item.quantity, 0) === 0 && (<p>Select a at least one item before proceeding to checkout!</p>)}
-  {shippingType === "Select a Shipping Method" && (<p>Select a shipping option before proceeding to checkout!</p>)}
-{shippingType !== "Select a Shipping Method" && selectedItems.reduce((total, item) => total + item.quantity, 0) > 0 && (
-<PayPalButton
-        amount={totalPrice} 
-        createOrder={(actions, data) => createOrder(actions, data)} 
-        onSuccess={(details) => handleOnSuccess(details)}
-      />)}
- </section>
- </div>
-  </div>
-    );
-};
-
 export default Checkout;  
-
