f5d860f652643760385d9a08cf01d32c630bd90e
JoanMilano <147004601+JoanMilano@users.noreply.github.com>
2024-03-02 15:28:37 -0800

    revision

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -228,7 +228,10 @@ const Checkout = () => {
       <div className="checkout-title"><h2>Checkout</h2></div>
 
       <section className="salve-option-one">
+      <div>
+<p className="salve-img-text">1oz tin starting at $17.99 </p>
       <img src={oneOz} alt="1oz tin of Black Salve" />
+      </div>
       <Dropdown className="dropdown">
   <button variant="info" className="btn btn-color btn-text">{firstButtonText} --1oz--</button>
   <DropdownToggle
@@ -237,26 +240,26 @@ const Checkout = () => {
    id="dropdown-split-basic" />
   <DropdownMenu className="drop-menu">
     <DropdownItem onClick={() => handleOnClick(productOne.title, 0, 0.00)} placeholder="1oz">None</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 1, 17.99)}>1 --1oz-- 17.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 2, 16.99)}>2 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 3, 16.99)}>3 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 4, 16.99)}>4 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 5, 16.99)}>5 --1oz-- 16.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 6, 15.99)}>6 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 7, 15.99)}>7 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 8, 15.99)}>8 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 9, 15.99)}>9 --1oz-- 15.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 10, 14.99)}>10 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 11, 14.99)}>11 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 12, 14.99)}>12 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 13, 14.99)}>13 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 14, 14.99)}>14 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 15, 14.99)}>15 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 16, 14.99)}>16 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 17, 14.99)}>17 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 18, 14.99)}>18 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 19, 14.99)}>19 --1oz-- 14.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productOne.title, 20, 14.99)}>20 --1oz-- 14.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 1, 17.99)}>1 --1oz-- $17.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 2, 16.99)}>2 --1oz-- $16.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 3, 16.99)}>3 --1oz-- $16.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 4, 16.99)}>4 --1oz-- $16.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 5, 16.99)}>5 --1oz-- $16.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 6, 15.99)}>6 --1oz-- $15.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 7, 15.99)}>7 --1oz-- $15.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 8, 15.99)}>8 --1oz-- $15.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 9, 15.99)}>9 --1oz-- $15.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 10, 14.99)}>10 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 11, 14.99)}>11 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 12, 14.99)}>12 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 13, 14.99)}>13 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 14, 14.99)}>14 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 15, 14.99)}>15 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 16, 14.99)}>16 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 17, 14.99)}>17 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 18, 14.99)}>18 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 19, 14.99)}>19 --1oz-- $14.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productOne.title, 20, 14.99)}>20 --1oz-- $14.99 ea.</DropdownItem>
     <DropdownDivider></DropdownDivider>
     <DropdownItem>Contact for Wholesale</DropdownItem>
   </DropdownMenu>
@@ -264,7 +267,10 @@ const Checkout = () => {
 </section>
 
 <section className="salve-option-two">
+  <div>
+<p className="salve-img-text">2oz tin starting at $24.99 </p>
 <img src={twoOz} alt="2oz tin of Black Salve" />
+</div>
 <Dropdown className="dropdown">
   <button variant="info" className="btn btn-color btn-text">{secondButtonText} --2oz--</button>
   <DropdownToggle
@@ -273,26 +279,26 @@ const Checkout = () => {
    id="dropdown-split-basic" />
   <DropdownMenu className="drop-menu">
   <DropdownItem onClick={() => handleOnClick(productTwo.title, 0, 0.00)} placeholder="2oz">None</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 1, 24.99)}>1 --2oz-- 24.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 2, 23.99)}>2 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 3, 23.99)}>3 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 4, 23.99)}>4 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 5, 23.99)}>5 --2oz-- 23.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 6, 22.99)}>6 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 7, 22.99)}>7 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 8, 22.99)}>8 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 9, 22.99)}>9 --2oz-- 22.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 10, 21.99)}>10 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 11, 21.99)}>11 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 12, 21.99)}>12 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 13, 21.99)}>13 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 14, 21.99)}>14 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 15, 21.99)}>15 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 16, 21.99)}>16 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 17, 21.99)}>17 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 18, 21.99)}>18 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 19, 21.99)}>19 --2oz-- 21.99$ ea.</DropdownItem>
-    <DropdownItem onClick={() => handleOnClick(productTwo.title, 20, 21.99)}>20 --2oz-- 21.99$ ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 1, 24.99)}>1 --2oz-- $24.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 2, 23.99)}>2 --2oz-- $23.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 3, 23.99)}>3 --2oz-- $23.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 4, 23.99)}>4 --2oz-- $23.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 5, 23.99)}>5 --2oz-- $23.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 6, 22.99)}>6 --2oz-- $22.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 7, 22.99)}>7 --2oz-- $22.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 8, 22.99)}>8 --2oz-- $22.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 9, 22.99)}>9 --2oz-- $22.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 10, 21.99)}>10 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 11, 21.99)}>11 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 12, 21.99)}>12 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 13, 21.99)}>13 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 14, 21.99)}>14 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 15, 21.99)}>15 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 16, 21.99)}>16 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 17, 21.99)}>17 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 18, 21.99)}>18 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 19, 21.99)}>19 --2oz-- $21.99 ea.</DropdownItem>
+    <DropdownItem onClick={() => handleOnClick(productTwo.title, 20, 21.99)}>20 --2oz-- $21.99 ea.</DropdownItem>
     <DropdownDivider></DropdownDivider> 
     <DropdownItem>Contact for WholeSale</DropdownItem> 
   </DropdownMenu>
@@ -320,9 +326,9 @@ const Checkout = () => {
   <h3>Order Details</h3>
     <p>{selectedItems.find(item => item.title === productOne.title)?.quantity || 0} --1oz-- {priceOfProductOnes.toFixed(2)}$</p>
      <p>{selectedItems.find(item => item.title === productTwo.title)?.quantity || 0} --2oz-- {priceOfProductTwos.toFixed(2)}$</p> 
-    <p>Subtotal: {subTotalPrice}$</p>
-    <p>{shippingType}: {shippingPrice}$</p>
-    <p>Total {totalPrice}$</p>
+    <p>Subtotal: ${subTotalPrice}</p>
+    <p>{shippingType}: ${shippingPrice}</p>
+    <p>Total ${totalPrice}</p>
  </section>
  </div>
  <section className="paypal-section">
